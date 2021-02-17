92558d74f7bae48a5525cf8764666de9ce26d105
Yashwanth <yashwanthdamera6@gmail.com>
2021-02-17 12:05:12 -0600

    User and location resource generate commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { QuestModule } from './quest/quest.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { LocationModule } from './location/location.module';
+
 
 @Module({
-  imports: [QuestModule],
+  imports: [QuestModule,TypeOrmModule.forRoot({
+    type: 'sqlite',
+    database: 'test.db',
+    synchronize: true,
+    entities: [],
+  }), UserModule, LocationModule],
   controllers: [AppController],
   providers: [AppService],
 })
